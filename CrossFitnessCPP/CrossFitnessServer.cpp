147b0732495ca563c26fd63dc6dcecc0efdf517e
Fabiola Marchì <mrcfbl99s48c342j@studium.unict.it>
2023-09-17 16:47:23 +0200

    da salvare

--- CrossFitnessCPP/CrossFitnessServer.cpp
+++ CrossFitnessCPP/CrossFitnessServer.cpp
@@ -1,20 +1,61 @@
 #include <iostream>
+#include<map>
 #include "crow.h"
 
 using namespace std;
-
+using json = nlohmann::json;
 class Prenotazione {
 public:
 
     int IDPrenotazione;
     int IDPersona;
     string Username;
+    string lezione;
     vector<string> LezioneList; 
+    map<int, string> LezioniDict;
+
+    int getIDPrenotazione(){ return IDPrenotazione; }
+    int getIDPersona(){ return IDPersona; }
+    string getUsername(){ return Username; }
+    string getLezioneStr() { return lezione; }
+
+    string getLezione()
+    {
+        string s = "";
+        for(int i =0; i < LezioneList.size(); i++)
+        {
+            s += LezioneList[i];
+            s += " ";
+        }
+        return s;
+    }
+    void setIDPrenotazione(int IDPren){ IDPrenotazione = IDPren; }
+
+    void setIDPersona(int IDPers){ IDPersona = IDPers; }
+
+    void setUsername(string name){ Username = name; }
+
+    void setLezioneStr(string lez) { lezione = lez; }
+
+    void setLezione(string lezione){LezioneList.push_back(lezione); }
+
+    void setLezioneDict(map<int, string> dict) { LezioniDict = dict; };
+
+    string showPrenotazione()
+    {
+        string s;
+        s = "ID Prenotazione : " + to_string(getIDPrenotazione()) + "\n" +
+            "ID Persona : " + to_string(getIDPersona()) + "\n" + "Username :" + getUsername() +
+            "\n" + "Prenotazione: " + getLezione() + "\n";
+        return s;
+    }       
 
     Prenotazione()
     {
 
     }
+       
+    
 };
 
 int main()
@@ -51,9 +92,20 @@ int main()
             });
 
     CROW_ROUTE(app, "/prenotazioni")
-        .methods("POST"_method,"GET"_method)([](const crow::request& req) {
+        .methods("POST"_method)([](const crow::request& req) {
+        Prenotazione p; 
 
         std::string Utente = req.body;
+
+        auto json_data = crow::json::load(req.body);
+        p.setUsername(json_data["Username:"].s());
+        p.setIDPrenotazione(json_data["IDPrenotazione:"].i());
+        p.setIDPersona(json_data["IDPersona:"].i()); 
+        p.setLezione(json_data["LezioneStr:"].s());
+        p.LezioneList.push_back(json_data["LezioneStr:"].s());
+
+        cout << "PRENOTAZIONE: \n" << p.showPrenotazione() << endl;
+     
         if (Utente == "")
             return crow::response(400);
         else
