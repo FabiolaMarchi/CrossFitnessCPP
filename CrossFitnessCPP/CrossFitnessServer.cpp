9c5c8b2be128bc4c942149ea49b3e612dc7a19b2
Fabiola Marchì <mrcfbl99s48c342j@studium.unict.it>
2023-09-21 20:36:44 +0200

    POST ok con lettura da file e scrittura su file

--- CrossFitnessCPP/CrossFitnessServer.cpp
+++ CrossFitnessCPP/CrossFitnessServer.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include<fstream>
 #include<map>
 #include "crow.h"
-
+#include "json.hpp"
 using namespace std;
 using json = nlohmann::json;
 class Prenotazione {
@@ -58,59 +59,123 @@ public:
     
 };
 
+map<string, string> caricaFile() {
+    
+    ifstream file("User.txt");
+    map<string, string> users;
+    string line, nome;
+    string psw;
+    while (std::getline(file, line)) {
+        istringstream iss(line);
+        getline(iss, nome, ';');
+        getline(iss, psw, ';');
+        users.insert({ nome, psw });
+    }
+    file.close();
+    return users;
+}
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
-
+    std::cout << "Hello World!\n";     
     crow::SimpleApp app;
+    
 
     CROW_ROUTE(app, "/json")
         ([] {
         crow::json::wvalue x({ {"zmessage", "Hello, World!"},
                               {"amessage", "Hello, World2!"} });
     return x;
-            });
+    });
 
     CROW_ROUTE(app, "/login")
         .methods("POST"_method, "GET"_method)([](const crow::request& req) {
+        string json_psw, json_nome, res;     
        
-        std::string x = req.body;
-        if (x == "User=Fabiola&Password%3A=1234" || x == "User=Matteo&Password%3A=9876")
-           return crow::response(200);
-        else 
+        auto json_data = crow::json::load(req.body);
+        json_nome = json_data["User:"].s();        
+        json_psw = json_data["Password:"].s();        
+
+        map<string, string> users = caricaFile();
+        cout << "ciao: \n" <<  endl;
+        ifstream file1(json_nome + ".txt");
+        
+        string line, nome;
+        
+        while (std::getline(file1, line)) {                       
+            res += line +  '\n';            
+        }
+        file1.close();
+        if (users.find(json_nome) != users.end() && users[json_nome] == json_psw)             
+            return crow::response(res);                     
+        else
             return crow::response(400);       
-            });
+       
+    });      
+             
 
     CROW_ROUTE(app, "/crea_account")
         .methods("POST"_method, "GET"_method)([](const crow::request& req) {
-
-        std::string Utente = req.body;
-        if (Utente == "")
+        string json_psw, json_nome, tmp;        
+        auto json_data = crow::json::load(req.body);
+        json_nome = json_data["User:"].s();
+        json_psw = json_data["Password:"].s();
+        tmp = json_nome + ";" + json_psw;
+        if (json_nome == "" && json_psw == "")
             return crow::response(400);
         else
+        {
+            ofstream file("User.txt", ios::app);
+            file.eof();
+            file << tmp;
+            file.close();
             return crow::response(200);
-            });
+        }
+            
+            
+    });
 
     CROW_ROUTE(app, "/prenotazioni")
-        .methods("POST"_method)([](const crow::request& req) {
-        Prenotazione p; 
-
+        .methods("POST"_method, "DELETE"_method)([](const crow::request& req) {
+        
+        Prenotazione p;
+        string tmp;
         std::string Utente = req.body;
+        if (req.method == "POST"_method) {
+            
+            auto json_data = crow::json::load(req.body);
+            p.setUsername(json_data["Username:"].s());
+            p.setIDPrenotazione(json_data["IDPrenotazione:"].i());
+            p.setIDPersona(json_data["IDPersona:"].i());
+            p.setLezione(json_data["LezioneStr:"].s());
+            string usr = json_data["Username:"].s();
+            int IDpr = json_data["IDPrenotazione:"].i();
+            int IDp = json_data["IDPersona:"].i();
+            string lez = json_data["LezioneStr:"].s();
+            
+            ofstream file(usr + ".txt", ios::app);
+            file.eof();
+            file << lez;
+            file.close();          
+                              
+            cout << "PRENOTAZIONE: \n" << p.showPrenotazione() << endl;
 
-        auto json_data = crow::json::load(req.body);
-        p.setUsername(json_data["Username:"].s());
-        p.setIDPrenotazione(json_data["IDPrenotazione:"].i());
-        p.setIDPersona(json_data["IDPersona:"].i()); 
-        p.setLezione(json_data["LezioneStr:"].s());
-        p.LezioneList.push_back(json_data["LezioneStr:"].s());
-
-        cout << "PRENOTAZIONE: \n" << p.showPrenotazione() << endl;
-     
-        if (Utente == "")
-            return crow::response(400);
-        else
+            if (Utente == "")
+                return crow::response(400);
+            else
+                return crow::response(200);
+        }
+        else if (req.method == "DELETE"_method)
+        {
+            ofstream fout("Prenotazione.txt");
+            fout << " ";
+            fout.close();            
             return crow::response(200);
-            });
+           
+        }
+    });
+        
 
     app.port(60080).run();
 }
